32572b6883dc9618aba62e4e9238e42836bd005f
Evan Sosenko <evan.sosenko@meltwater.com>
2020-09-21 15:25:45 -0700

    Use createSsmStringConfigurationRequest

--- handlers/head/config.js
+++ handlers/head/config.js
@@ -1,23 +1,9 @@
-import ac from 'argument-contracts'
 import {
-  ConfigurationRequest,
-  ConfigurationRequestTypes
+  createSsmStringConfigurationRequest
 } from '@meltwater/aws-configuration-fetcher'
 
-// UPSTREAM: Want this function.
-const createStringConfigurationRequest = (propertyName, key) =>
-  new ConfigurationRequest({
-    adapter: (value) => {
-      ac.assertNonWhiteSpaceString(value, propertyName)
-      return value
-    },
-    key,
-    propertyName,
-    type: ConfigurationRequestTypes.ssm
-  })
-
 export const configurationRequests = [
-  createStringConfigurationRequest(
+  createSsmStringConfigurationRequest(
     'tailLambdaArn',
     process.env.SSM_TAIL_LAMBDA_ARN
   )
