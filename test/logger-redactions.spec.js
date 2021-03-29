f36fd0d147b21cbcf876bf68ead86a669b92bce3
Drew Mulock <drew.mulock@meltwater.com>
2021-03-29 14:47:19 -0700

    Fix brack notation redactions

--- test/logger-redactions.spec.js
+++ test/logger-redactions.spec.js
@@ -1,6 +1,14 @@
 import test from 'ava'
+import { createLogger } from '@meltwater/mlabs-logger'
 import { LOGGER_REDACTION_LISTS } from '../lib/logger-redactions'
 
 test('snapshot of redactions', async (t) => {
   t.snapshot(LOGGER_REDACTION_LISTS)
+})
+
+test('should not throw "pino – redact paths array contains an invalid path"', async (t) => {
+  Object.values(LOGGER_REDACTION_LISTS).forEach((redact) =>
+    createLogger({ redact })
+  )
+  t.pass()
 })
